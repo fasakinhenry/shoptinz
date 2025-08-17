6a7b6db961734bf17d0fbc4352ead9cce7f50cfe
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-17 18:33:04 +0100

    feat: create the Hero

--- client/src/components/landing/hero/index.jsx
+++ client/src/components/landing/hero/index.jsx
@@ -1,5 +1,26 @@
-const hero = () => {
-  return <div>hero</div>;
+import Button from '@components/button';
+import './hero.css';
+
+const Hero = () => {
+  return (
+    <section className='relative py-16 text-center'>
+      <div className='hero-gradient'></div>
+      <h1 className='text-4xl font-bold mb-4'>
+        Easily manage, track & find your affiliate partners.
+      </h1>
+      <p className='text-lg mb-6'>
+        ProfitPulse helps businesses, manage, track, and find affiliates.
+      </p>
+      <div className='space-x-4'>
+        <Button variant='primary' size='md'>
+          Get Started
+        </Button>
+        <Button variant='secondary' size='md'>
+          Book Demo
+        </Button>
+      </div>
+    </section>
+  );
 };
 
-export default hero;
+export default Hero;
