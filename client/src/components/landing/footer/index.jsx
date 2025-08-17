a018e2ee7fbb8bfe0797b491de429a2a21f0f339
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-17 23:58:00 +0100

    feat: create the footer section

---
+++ client/src/components/landing/footer/index.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className='py-8 text-center text-gray-500'>
+      <div className='text-4xl font-bold mb-4'>trackit.</div>
+      <div className='space-x-4'>
+        <a href='#'>Product</a>
+        <a href='#'>Features</a>
+        <a href='#'>Company</a>
+        <a href='#'>Facebook</a>
+        <a href='#'>Instagram</a>
+        <a href='#'>X</a>
+      </div>
+      <p className='mt-4'>© 2025, trackit. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
