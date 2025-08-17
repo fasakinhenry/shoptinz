215c0a91b8173a9132097d979680eaa116ce5620
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-18 00:01:13 +0100

    fix: fix button import errors

--- client/src/routes/landingPage/index.jsx
+++ client/src/routes/landingPage/index.jsx
@@ -1,12 +1,29 @@
-import Header from '@/components/landing/header';
-import Hero from '@/components/landing/hero';
+import Header from '@components/landing/header';
+import Hero from '@components/landing/hero';
+import Sponsors from '@components/landing/sponsors';
+import Features from '@components/landing/features';
+import Stats from '@components/landing/stats';
+import Marketplace from '@components/landing/marketplace';
+import Revenue from '@components/landing/revenue';
+import Pricing from '@components/landing/pricing';
+import CallToAction from '@components/landing/callToAction';
+import Footer from '@components/landing/footer';
+
 const LandingPage = () => {
   return (
-    <div className='min-h-screen w-full overflow-x-hidden bg-gray-50 text-gray-800'>
+    <div className="min-h-screen w-full overflow-x-hidden bg-gray-50 text-gray-800">
       <Header />
-      <div className='container mx-auto px-4 flex flex-col'>
+      <div className="container mx-auto px-4">
         <Hero />
+        <Sponsors />
+        <Features />
+        <Stats />
+        <Marketplace />
+        <Revenue />
+        <Pricing />
+        <CallToAction />
       </div>
+      <Footer />
     </div>
   );
 };
