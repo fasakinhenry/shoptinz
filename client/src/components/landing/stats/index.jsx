0d3e18d7b61793d5303cd86c4aaf2f22d3d9400a
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-17 23:54:30 +0100

    feat: create the stats section

---
+++ client/src/components/landing/stats/index.jsx
@@ -0,0 +1,27 @@
+const Stats = () => {
+  return (
+    <section className='py-16 text-center'>
+      <h2 className='text-3xl font-bold mb-8'>Trusted & loved</h2>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+        <div>
+          <h3 className='text-2xl font-bold'>20s</h3>
+          <p>Of Networks</p>
+        </div>
+        <div>
+          <h3 className='text-2xl font-bold'>97%</h3>
+          <p>Customer satisfied with our service</p>
+        </div>
+        <div>
+          <h3 className='text-2xl font-bold'>15B+</h3>
+          <p>Events / Month</p>
+        </div>
+        <div>
+          <h3 className='text-2xl font-bold'>03</h3>
+          <p>Payment month duration</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
