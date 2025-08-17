6f263ec7f30b0a3a134d9f43eb66e4c377caa0b9
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-17 23:52:42 +0100

    feat: add the sponsors section

---
+++ client/src/components/landing/sponsors/index.jsx
@@ -0,0 +1,16 @@
+const Sponsors = () => {
+  return (
+    <section className='py-10 text-center'>
+      <p className='text-gray-500'>200+ companies use ProfitPulse for growth</p>
+      <div className='flex justify-center space-x-8 mt-4'>
+        <span>Sider</span>
+        <span>trip.com</span>
+        <span>AccoJob</span>
+        <span>PHILIPS</span>
+        <span>airbnb</span>
+      </div>
+    </section>
+  );
+};
+
+export default Sponsors;
