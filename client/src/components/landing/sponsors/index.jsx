b0e1036564dc17b59b65a46c218932b9765d572f
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-24 19:30:58 +0100

    style: add styles for the sponsors section

--- client/src/components/landing/sponsors/index.jsx
+++ client/src/components/landing/sponsors/index.jsx
@@ -1,13 +1,12 @@
 const Sponsors = () => {
+  const sponsorList = ["Ebay", "Microsoft", "Airbnb", "Sider", "Daytrip", "Vodaphone", "Acciojobs", "Philips"]
   return (
-    <section className='py-10 text-center'>
+    <section className='py-8 text-center'>
       <p className='text-gray-500'>200+ companies use ProfitPulse for growth</p>
-      <div className='flex justify-center space-x-8 mt-4'>
-        <span>Sider</span>
-        <span>trip.com</span>
-        <span>AccoJob</span>
-        <span>PHILIPS</span>
-        <span>airbnb</span>
+      <div className='flex justify-center space-x-4 mt-4'>
+        {sponsorList.map((sponsor) => (
+          <span className="px-5 py-1 shadow-xl border-white border-2 rounded-md bg-gray-100 font-medium">{sponsor}</span>
+        ))}
       </div>
     </section>
   );
