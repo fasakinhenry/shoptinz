d91cde89c3e16898dd766ee76b70f4789e1b9613
Fasakin Henry <fasakinhenry@gmail.com>
2025-09-01 16:38:29 +0100

    fix: fix issue with font imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import LandingPage from '@/routes/landingPage';
 import './index.css';
+import AuthPage from './routes/authPage/authPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/auth' element={<AuthPage />} />
       </Routes>
     </BrowserRouter>
   );
